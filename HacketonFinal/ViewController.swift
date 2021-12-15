1c8055657dfcee7b475de8d65511f6b3ade504cc
Kewal Patel <kaakaa135@gmail.com>
2021-12-14 21:25:46 -0500

    All buttons included

--- HacketonFinal/ViewController.swift
+++ HacketonFinal/ViewController.swift
@@ -58,43 +58,177 @@ class ViewController: UIViewController {
             {
                 label.text = "+";
             }
+            
             operation = sender.tag
             performingMath = true;
         }
         else if sender.tag == 16
         {
-            if operation == 12{
+            if operation == 12
+            {
+            label.text = String(previousNumber / numberOnScreen)
                 
-                label.text = String(previousNumber / numberOnScreen)
-
             }
             else if operation == 13{
-                
-                label.text = String(previousNumber * numberOnScreen)
+                    
+            label.text = String(previousNumber * numberOnScreen)
 
             }
             else if operation == 14{
                 
-                label.text = String(previousNumber - numberOnScreen)
+            label.text = String(previousNumber - numberOnScreen)
 
             }
             else if operation == 15{
-                label.text = String(previousNumber + numberOnScreen)
+                
+            label.text = String(previousNumber + numberOnScreen)
             }
 
         }
-        else if sender.tag == 11{
-            label.text = ""
-            previousNumber = 0;
-            numberOnScreen = 0;
-            operation = 0;
+            
+            else if sender.tag == 11{
+                label.text = ""
+                previousNumber = 0;
+                numberOnScreen = 0;
+                operation = 0;
+            }
+        }
+    
+    @IBAction func plusminusButton(_ sender: UIButton) {
+        if label.text == "0" || label.text == "" {
+                return
+            }
+            if !label.text!.contains("-") {
+                label.text!.insert("-", at: label.text!.startIndex)
+                
+            } else {
+                label.text!.remove(at: label.text!.startIndex)
+                
+            }
+        }
+    
+    @IBAction func advanceButtons(_ sender: UIButton) {
+        if sender.tag == 17 // "("
+        {
+            label.text = "(";
+        }
+        else if sender.tag == 18 // ")"
+        {
+            label.text = ")";
+        }
+        else if sender.tag == 19 // "mc"
+        {
+            label.text = "mc";
+        }
+        else if sender.tag == 20 // "m+"
+        {
+            label.text = "m+";
+        }
+        else if sender.tag == 21 // advance
+        {
+            label.text = "m-";
+        }
+        else if sender.tag == 22 // advance
+        {
+            label.text = "mr";
+        }
+        else if sender.tag == 23 // advance
+        {
+            label.text = "2nd";
+        }
+        else if sender.tag == 24 // advance
+        {
+            label.text = "x2";
+        }
+        else if sender.tag == 25 // advance
+        {
+            label.text = "x3";
+        }
+        else if sender.tag == 26 // advance
+        {
+            label.text = "xy";
+        }
+        else if sender.tag == 27 // advance
+        {
+            label.text = "ex";
+        }
+        else if sender.tag == 28 // advance
+        {
+            label.text = "10x";
+        }
+        else if sender.tag == 29 // advance
+        {
+            label.text = "1/x";
+        }
+        else if sender.tag == 30 // advance
+        {
+            label.text = "2/x";
+        }
+        else if sender.tag == 31 // advance
+        {
+            label.text = "3/x";
+        }
+        else if sender.tag == 32 // advance
+        {
+            label.text = "x/x";
+        }
+        else if sender.tag == 33 // advance
+        {
+            label.text = "In";
+        }
+        else if sender.tag == 34 // advance
+        {
+            label.text = "log10";
+        }
+        else if sender.tag == 35 // advance
+        {
+            label.text = "x!";
+        }
+        else if sender.tag == 36 // advance
+        {
+            label.text = "sin";
+        }
+        else if sender.tag == 37 // advance
+        {
+            label.text = "cos";
+        }
+        else if sender.tag == 38 // advance
+        {
+            label.text = "tan";
+        }
+        else if sender.tag == 39 // advance
+        {
+            label.text = "e";
+        }
+        else if sender.tag == 40 // advance
+        {
+            label.text = "EE";
+        }
+        else if sender.tag == 41 // advance
+        {
+            label.text = "Rad";
+        }
+        else if sender.tag == 42 // advance
+        {
+            label.text = "sinh";
+        }
+        else if sender.tag == 43 // advance
+        {
+            label.text = "cosh";
+        }
+        else if sender.tag == 44 // advance
+        {
+            label.text = "tanh";
+        }
+        else if sender.tag == 45 // advance
+        {
+            label.text = "pi";
+        }
+        else if sender.tag == 46 // advance
+        {
+            label.text = "Rand";
         }
         
     }
-    
-    
-    
-    
-    
-    
 }
+
